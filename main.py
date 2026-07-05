4435ed436463557338e5e6aa1a008a4a3d8f45d5
nox-vixen <nox.vixen.0@gmail.com>
2026-07-05 11:50:34 +0530

    Inspect moviebox v3 modules

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from fastapi import FastAPI
-import moviebox_api.v3 as v3
+import pkgutil
+import moviebox_api.v3
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
     return {
-        "exports": dir(v3)
+        "modules": [
+            m.name for m in pkgutil.iter_modules(moviebox_api.v3.__path__)
+        ]
     }
