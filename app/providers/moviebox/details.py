003d987577806e8b380260e0e58e9ad1736eb2fa
nox-vixen <nox.vixen.0@gmail.com>
2026-07-05 13:18:32 +0530

    Connect home route

--- app/providers/moviebox/details.py
+++ app/providers/moviebox/details.py
@@ -1,1 +1,12 @@
 from moviebox_api.v3 import MovieBoxHttpClient
+from moviebox_api.v3.core import ItemDetails
+
+
+async def get_details(subject_id: str):
+    async with MovieBoxHttpClient() as client:
+        details = ItemDetails(
+            client_session=client,
+            include_seasons=True,
+        )
+
+        return await details.get_content(subject_id)
