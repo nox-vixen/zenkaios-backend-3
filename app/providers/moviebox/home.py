32988bc367aacea65c3e957040cddb2d9f572aa5
nox-vixen <nox.vixen.0@gmail.com>
2026-07-05 11:38:41 +0530

    Inspect homepage structure

--- app/providers/moviebox/home.py
+++ app/providers/moviebox/home.py
@@ -1,11 +1,13 @@
 from moviebox_api.v3.http_client import MovieBoxHttpClient
 from moviebox_api.v3.core import Homepage
 
-from .adapter import normalize_home
-
 
 async def get_home():
     async with MovieBoxHttpClient() as client:
         home = Homepage(client)
         data = await home.get_content()
-        return normalize_home(data)
+
+        return {
+            "first_item": data["items"][0],
+            "second_item": data["items"][1]
+        }
